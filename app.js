7a770df740156b56c4a79a1ae1944ed23dc5e3d1
vattanac <vattanacsim99@gmail.com>
2019-05-27 00:07:41 +0700

    ep2

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 const express = require('express');
 const app = express();
-
 //log server 
 const morgan = require('morgan');
+const mysql = require('mysql');
 
-app.use(morgan('short'));
+app.use(morgan('combined'));
 
+app.get('/user/:id', (req, res) => {
+    console.log("fetching user " + req.params.id);
+    
+        const connection = mysql.createConnection({
+            host:'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'mynode_db',
+            port: 8889 //if not put this statement it get nothing from database
+        });
+
+        const userId = req.params.id;
+        const queryString = "SELECT * FROM users WHERE id = ?";
+        connection.query(queryString, [userId], (err,rows,fields) => {
+            
+            if (err) {
+                //res.status(500).send({error: 'something failed'});
+                res.sendStatus(500);
+                return;
+            }
+
+            //change field name in database to display ex: u_id -> id
+            const users = rows.map((row) => {
+                return {firstname: row.first_name, lastname: row.last_name};
+            });
+            res.json(users);
+            
+        });
+          
+});
 
 app.get("/", (req, res) => {
     console.log("Responding root route");
